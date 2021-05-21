0058d4e4d1bae61440d7601d68e632447b880bbc
ghkdwjdrk123 <ghkdwjdrk123@naver.com>
2021-05-21 17:28:26 +0900

    0521_crime

---
+++ src/main/java/com/ssafy/happyhouse/model/service/CrimePopulService.java
@@ -0,0 +1,12 @@
+package com.ssafy.happyhouse.model.service;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface CrimePopulService {
+
+	Map<String, Integer> getCrimePopul(String siname);
+	
+}
